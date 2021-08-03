488b01b46774dd21c4463453276e7d17efc6e881
unknown <sandeepshinde630@gmail.com>
2021-08-04 00:03:14 +0530

    feat:Find out Length

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
 namespace LineComparisonProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Problem!");
+            int x1, x2, y1, y2;
+            x1 = 11;
+            x2 = 14;
+            y1 = 16;
+            y2 = 18;
+            var distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine(distance);
         }
     }
 }
